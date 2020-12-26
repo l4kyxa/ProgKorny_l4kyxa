b085d6d342224b67f629bc6855a3ef233c1c460a
l4kyxa <l4kyxa@gmail.com>
2020-12-26 09:05:31 +0100

    2020-12-26(09:05)

---
+++ ProgKorny_l4kyxa/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgKorny_l4kyxa.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
